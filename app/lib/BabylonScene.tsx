55e196d2de636f261aa249548e2b0da8946432c5
ProgrammeChef <superbapple0@gmail.com>
2023-10-31 12:46:16 -0400

    Add BabylonScene.tsx file
    This file is a component for using only rendering GLB file.

---
+++ app/lib/BabylonScene.tsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default function BabylonScene() {
+    return (
+        <div className="container-fluid p-2 text-center">
+            <button type="button" className="btn btn-success">
+                Babylon Scene
+            </button>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+// import Image from 'next/image'
+// import styles from './page.module.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import BabylonScene from './lib/BabylonScene'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <button type="button" className="btn btn-success">
         Import GLB File
       </button>
+      <BabylonScene />
     </div>
   )
 }
