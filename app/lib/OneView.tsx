403c810c7220a8c0a2f878d73a1bfa3fcf5432a6
ProgrammeChef <superbapple0@gmail.com>
2023-11-01 14:52:06 -0400

    Delete another files.

--- app/lib/OneView.tsx
+++ app/lib/OneView.tsx
@@ -1,41 +1,71 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import * as BABYLON from 'babylonjs'
 import * as Materials from 'babylonjs-materials';
 
 const OneView = () => {
+    const [GLBBase64String, setGLBBase64String] = useState("");
+    const [customGLBSetFlag, setCustomGLBSetFlag] = useState(false);
+
     const glbFileRef = useRef(null);
+
+    let canvas, engine, scene, camera, light, file;
+
+    function fileToBase64(file, callback) {
+        var reader = new FileReader();
+        reader.onload = function(event) {
+          var base64String = event.target.result.split(',')[1];
+          console.log("base64String: ", base64String);
+          callback(base64String);
+        };
+        reader.readAsDataURL(file);
+    }
 
     const handleFileChange = () => {
         console.log("File: ", glbFileRef.current.files[0]);
+        file = glbFileRef.current.files[0];
+        if (file) {
+            fileToBase64(file, base64String => {
+                setGLBBase64String(base64String);
+                setTimeout(() => {
+                    setCustomGLBSetFlag(true);
+                }, 200);
+            })
+        }
     }
 
     useEffect(() => {
 
-        const canvas = document.getElementById('ViewCanvas');
-        const engine = new BABYLON.Engine(canvas, true);
+        canvas = document.getElementById('ViewCanvas');
+        engine = new BABYLON.Engine(canvas, true);
         
         //////////////////////////////// Scene Start ////////////////////////////////
 
-        const scene = new BABYLON.Scene(engine);
-        const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+        scene = new BABYLON.Scene(engine);
+        camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
         camera.setTarget(BABYLON.Vector3.Zero());
         camera.attachControl(canvas, true);
-        const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+        light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
         light.intensity = 0.7;
-    
-        // Sample Sphere
-        const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
-        
-        // Sphere Material
-        const mySphereMat = new BABYLON.StandardMaterial("mySphereMat", scene);
-        mySphereMat.diffuseColor = new BABYLON.Color3(1, 0, 0);
-        sphere.material = mySphereMat;
-    
-        sphere.position.y = 1;
-    
-        // Sample Ground
-        const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
 
+        if (!customGLBSetFlag) {        
+            // Sample Sphere
+            const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
+            
+            // Sphere Material
+            const mySphereMat = new BABYLON.StandardMaterial("mySphereMat", scene);
+            mySphereMat.diffuseColor = new BABYLON.Color3(1, 0, 0);
+            sphere.material = mySphereMat;
+        
+            sphere.position.y = 1;
+
+            // Sample Ground
+            const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+        }
+
+        if (customGLBSetFlag) {
+            // If the user selects the GLB file to view it.
+        }
+        
         //////////////////////////////// Scene End ////////////////////////////////
 
         engine.runRenderLoop(() => {
@@ -46,7 +76,7 @@ const OneView = () => {
           engine.dispose();
         };
 
-    }, []);
+    }, [customGLBSetFlag]);
 
     return (
         <div className="row">
